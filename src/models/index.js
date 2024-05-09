7d74fa2ee900bf774464f4cc699bce88955d15ec
Mohd Afri <mohammadsaeedafri9@gmail.com>
2024-05-10 00:34:52 +0530

    modified:   src/models/index.js

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,6 @@ const Document = require("./document");
 const Event = require("./event");
 const Guest = require("./guest");
 const Location = require("./location");
-const User = require("./user");
 
 module.exports = {
   Document,
