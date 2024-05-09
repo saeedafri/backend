f076e22f52522ed412fe63bb1515d4a87e6ba38d
Mohd Afri <mohammadsaeedafri9@gmail.com>
2024-05-10 02:18:30 +0530

    Enabled Logging

--- src/config/setUpDatabase.js
+++ src/config/setUpDatabase.js
@@ -4,26 +4,22 @@ const logger = require("../utils/logger");
 require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.DB_URL, {
-  logging: false,
+  logging: true,
 });
 
 async function setupDatabase() {
   try {
     const dbName = process.env.DB_NAME;
 
-    // Drop the database
     const dropCommand = `DROP DATABASE IF EXISTS ${dbName};`;
     await sequelize.query(dropCommand);
 
-    // Create the database
     const createCommand = `CREATE DATABASE IF NOT EXISTS ${dbName};`;
     await sequelize.query(createCommand);
 
-    // Run migrations
     const migrateCommand = "npx sequelize-cli db:migrate";
     execSync(migrateCommand, { stdio: "inherit" });
 
-    // Seed the database
     const seedCommand = "npx sequelize-cli db:seed:all";
     execSync(seedCommand, { stdio: "inherit" });
   } catch (error) {
