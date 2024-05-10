ea7e41076cb13e9b1fd71ce9db9e909355a140f4
Mohd Afri <mohammadsaeedafri9@gmail.com>
2024-05-10 21:50:28 +0530

    Changes new

--- config/config.js
+++ config/config.js
@@ -11,14 +11,14 @@ module.exports = {
   test: {
     username: process.env.DB_USER || "root",
     password: process.env.DB_PASS || "admin",
-    database: process.env.DB_NAME_TEST || "railway",
+    database: process.env.DB_NAME || "railway",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql",
   },
   production: {
     username: process.env.DB_USER || "root",
     password: process.env.DB_PASS || "admin",
-    database: process.env.DB_NAME_PROD || "railway",
+    database: process.env.DB_NAME || "railway",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql",
   },
