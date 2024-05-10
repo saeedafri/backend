2dbc22363cda88240066cec5d6d1c38fff256071
Mohd Afri <mohammadsaeedafri9@gmail.com>
2024-05-10 22:06:55 +0530

    Changes DB

--- src/config/setUpDatabase.js
+++ src/config/setUpDatabase.js
@@ -19,26 +19,6 @@ const sequelize = new Sequelize(
 async function setupDatabase() {
   try {
     const dbName = process.env.DB_NAME;
-
-    // Log the start of the database setup process
-    logger.info(`Setting up database: ${dbName}`);
-
-    const dropCommand = `DROP DATABASE IF EXISTS ${dbName};`;
-    try {
-      await sequelize.query(dropCommand);
-      logger.info(`Dropped database: ${dbName}`);
-    } catch (dropError) {
-      logger.error(`Failed to drop database: ${dbName}`, dropError);
-    }
-
-    const createCommand = `CREATE DATABASE IF NOT EXISTS ${dbName};`;
-    try {
-      await sequelize.query(createCommand);
-      logger.info(`Created database: ${dbName}`);
-    } catch (createError) {
-      logger.error(`Failed to create database: ${dbName}`, createError);
-    }
-
     const migrateCommand = "npx sequelize-cli db:migrate";
     try {
       execSync(migrateCommand, { stdio: "inherit" });
